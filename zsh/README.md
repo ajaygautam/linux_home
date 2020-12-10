fdbc816901256a9e8060d55e38e9aef8ebd64155
Ajay Gautam <ajay610@gmail.com>
2020-12-10 08:16:08 -0500

    first-try

---
+++ zsh/README.md
@@ -0,0 +1,8 @@
+get this into workspace/linux_home
+
+Install ohmyzsh: `sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`
+
+Add to ~/.zshrc at the bottom:
+```
+source $HOME/workspace/linux_home/zsh/zshrc.sh
+```
---
+++ zsh/zshrc.sh
@@ -0,0 +1,8 @@
+alias df='df -h'
+alias grep='grep --color'                     # show differences in colour
+alias egrep='egrep --color=auto'              # show differences in colour
+alias fgrep='fgrep --color=auto'              # show differences in colour
+
+export EXINIT="set number tabstop=4 autoindent ignorecase hlsearch"
+
+export PATH=$PATH:$HOME/tools/flutter/bin:$HOME/bin
